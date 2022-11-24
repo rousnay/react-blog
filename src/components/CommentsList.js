418ecb56c0e2496028dbff158985dd979197cd2d
Mozahidur Rahman <mr.rousnay@gmail.com>
2022-11-24 20:13:27 +0600

    CommentsList component added

---
+++ src/components/CommentsList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import shortid from "shortid";
+const CommentsList = ({ comments }) => {
+  return (
+    <>
+      <h3>Comments:</h3>
+      {comments.map((comment) => {
+        return (
+          <div className="comment" key={shortid.generate()}>
+            <h4>{comment.postedBy}</h4>
+            <p>{comment.text}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default CommentsList;
